8baa0ca1db46cfae0cc0cbd797a074762d04221c
Dev Prajapat <devprajapat02@gmail.com>
2023-02-02 14:35:00 +0530

    fixed Data Vault user interface

--- lib/ui/dashboard/data_vault_screen/data_vault_screen.dart
+++ lib/ui/dashboard/data_vault_screen/data_vault_screen.dart
@@ -41,35 +41,40 @@ class _DataVaultScreenState extends State<DataVaultScreen> {
             ? const Center(
                 child: CircularProgressIndicator(),
               )
-            : ListView(
-                padding: const EdgeInsets.only(
-                    top: 20, left: 10, right: 10, bottom: 100),
-                children: [
-                  userVault("First Name", controller.dataVaultModel!.firstName),
-                  userVault("Last Name", controller.dataVaultModel!.lastName),
-                  userVault("Username", controller.dataVaultModel!.username),
-                  userVault("Sex", controller.dataVaultModel!.sex),
-                  userVault("DOB", controller.dataVaultModel!.dob),
-                  userVault("Address", controller.dataVaultModel!.address),
-                  userVault("City", controller.dataVaultModel!.city),
-                  userVault(
-                      "HealthCareID", controller.dataVaultModel!.healthCareId),
-                  userVault(
-                      "PostalCode", controller.dataVaultModel!.postalCode),
-                  userVault("patientGroups",
-                      controller.dataVaultModel!.patientGroups[0].name),
-                  userVault("clinician", controller.dataVaultModel!.clinician),
-                  userVault(
-                      "Total Measurements",
-                      controller.dataVaultModel!.measurements.measurments.length
-                          .toString()),
-                  userVault(
-                      "Total Questionnaires",
-                      controller.dataVaultModel!.questionnaires
-                          .questionnairesListJson.results.length
-                          .toString()),
-                ],
-              ),
+            : Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(100),
+                ),
+                child: ListView(
+                  padding: const EdgeInsets.only(
+                      top: 20, left: 10, right: 10, bottom: 100),
+                  children: [
+                    userVault("First Name", controller.dataVaultModel!.firstName),
+                    userVault("Last Name", controller.dataVaultModel!.lastName),
+                    userVault("Username", controller.dataVaultModel!.username),
+                    userVault("Sex", controller.dataVaultModel!.sex),
+                    userVault("DOB", controller.dataVaultModel!.dob),
+                    userVault("Address", controller.dataVaultModel!.address),
+                    userVault("City", controller.dataVaultModel!.city),
+                    userVault(
+                        "Health Care ID", controller.dataVaultModel!.healthCareId),
+                    userVault(
+                        "Postal Code", controller.dataVaultModel!.postalCode),
+                    userVault("Patient Groups",
+                        controller.dataVaultModel!.patientGroups[0].name),
+                    userVault("clinician", controller.dataVaultModel!.clinician),
+                    userVault(
+                        "Total Measurements",
+                        controller.dataVaultModel!.measurements.measurments.length
+                            .toString()),
+                    userVault(
+                        "Total Questionnaires",
+                        controller.dataVaultModel!.questionnaires
+                            .questionnairesListJson.results.length
+                            .toString()),
+                  ],
+                ),
+            ),
       ),
     );
   }
@@ -81,25 +86,27 @@ class _DataVaultScreenState extends State<DataVaultScreen> {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
+            padding: const EdgeInsets.all(10.0),
             //width: MediaQuery.of(context).size.width * 0.45,
-            width: 191,
+            width: 194,
             height: MediaQuery.of(context).size.height * 0.05,
             color: Colors.blueGrey.shade200,
             child: Text(
-              label,
+              label[0].toUpperCase() + label.substring(1, label.length),
               maxLines: 2,
-              style: themeTextStyle(context: context),
+              style: themeTextStyle(context: context, fweight: FontWeight.w200, fontFamily: "Raleway"),
             ),
           ),
           Container(
+            padding: const EdgeInsets.all(10.0),
             //width: MediaQuery.of(context).size.width * 0.45,
-            width: 200,
+            width: 197,
             height: MediaQuery.of(context).size.height * 0.05,
             color: Colors.blueGrey.shade100,
             child: Text(
-              data,
+              data[0].toUpperCase() + data.substring(1, data.length),
               maxLines: 2,
-              style: themeTextStyle(context: context),
+              style: themeTextStyle(context: context, fweight: FontWeight.w500, fontFamily: "Arial"),
             ),
           ),
         ],
